89c915faa788927a594a0a52ec12074db82b5852
Jacob Schmidt <innovativestudios@outlook.com>
2024-10-11 20:05:27 -0500

    Updated Icons

--- mod.cpp
+++ mod.cpp
@@ -1,21 +1,16 @@
 dir = "@sof";
-name = "SOF Mod";
-
-author = "SOF";
-
-picture         = "title_co.paa";           // Picture displayed from the expansions menu. Optimal size is 2048x1024
-hideName        = "false";                  // Hide the extension name
-hidePicture     = "false";                  // Hide the extension menu
-
-logoSmall       = "icon_64_ca.paa";         // Display next to the item added by the mod 64x64
-logo            = "icon_128_ca.paa";        // Logo displayed in the main menu 128x128
-logoOver        = "icon_128_highlight_ca.paa"; // When the mouse is over, in the main menu 128x128
+name = "PMC Simulator 3.0";
+author = "Jacob Schmidt";
+picture         = "title_co.paa";
+hideName        = "false";
+hidePicture     = "false";
+logoSmall       = "icon_64_ca.paa";
+logo            = "icon_128_ca.paa";
+logoOver        = "icon_128_highlight_ca.paa";
 tooltip         = "Tip of the morning";
-tooltipOwned    = "Special Operations Group";  // Tool tip displayed when the mouse is left over, in the main menu
-overview        = "Special Operations Group Official Modification";         // Overview text, displayed from the extension menu
-description     = "It's unclear where this will show"; // Probably in context with action
-action          = "https://a3sof.org";    // Website URL, that can accessed from the expansions menu 
-actionName      = "today?";                 // label of button/tooltip in extension menu
-
-// Color used for DLC stripes and backgrounds (RGBA)
+tooltipOwned    = "PMC Simulator 3.0";
+overview        = "PMC Simulator 3.0 Official Modification";
+description     = "It's unclear where this will show";
+action          = "https://innovativedevsolutions.org/";
+actionName      = "Website";
 dlcColor[] = {0.45, 0.47, 0.41, 1};
